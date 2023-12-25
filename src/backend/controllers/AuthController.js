414ec4f504da2fed6edd6a12a42d565d3b48fb38
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2023-12-25 10:04:18 +0530

    Revert "fix: auth controller"
    This reverts commit 97ac8d2d1073762ee7e47ada2ad393e871a5a38c.

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -2,23 +2,21 @@ import { v4 as uuid } from "uuid";
 import { Response } from "miragejs";
 import { formatDate } from "../utils/authUtils";
 import bcrypt from "bcryptjs";
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken"; 
 
-
-export const signupHandler = function (schema, request) {
+export const signupHandler = async function (request, res) {
   const { email, password, ...rest } = JSON.parse(request.requestBody);
   try {
-    // check if email already exists
-    const foundUser = schema.users.findBy({ email });
+    // Check if email already exists
+    const foundUser = res.users.findBy({ email });
     if (foundUser) {
-      return new Response(
-        422,
-        {},
+      return new Response(422, {},
         {
           errors: ["Unprocessable Entity. Email Already Exists."],
         }
       );
     }
+
     const _id = uuid();
     const encryptedPassword = bcrypt.hashSync(password, 5);
     const newUser = {
@@ -32,28 +30,20 @@ export const signupHandler = function (schema, request) {
       wishlist: [],
       address: [],
     };
-    const createdUser = schema.users.create(newUser);
-    // const encodedToken = jwt.sign(
-    //   { _id, email },
-    //   process.env.REACT_APP_JWT_SECRET
-    // );
-    return new Response(201, {}, { createdUser });
+
+    const createdUser = res.users.create(newUser);
+    const encodedToken = jwt.sign({ _id, email }, process.env.REACT_APP_JWT_SECRET);
+
+    return new Response(201, {}, { createdUser, encodedToken });
   } catch (error) {
-    return new Response(
-      500,
-      {},
-      {
-        error,
-      }
-    );
+    return new Response(500, {}, { error });
   }
 };
 
-
-export const loginHandler = function (schema, request) {
+export const loginHandler = async function (request, res) {
   const { email, password } = JSON.parse(request.requestBody);
   try {
-    const foundUser = schema.users.findBy({ email });
+    const foundUser = res.users.findBy({ email });
     if (!foundUser) {
       return new Response(
         404,
@@ -61,30 +51,21 @@ export const loginHandler = function (schema, request) {
         { errors: ["The email you entered is not Registered. Not Found error"] }
       );
     }
+
     if (bcrypt.compareSync(password, foundUser.password)) {
-      // const encodedToken = jwt.sign(
-      //   { _id: foundUser._id, email },
-      //   process.env.REACT_APP_JWT_SECRET
-      // );
+      const encodedToken = jwt.sign(
+        { _id: foundUser._id, email },
+        process.env.REACT_APP_JWT_SECRET
+      );
       foundUser.password = undefined;
-      return new Response(200, {}, { foundUser });
+
+      return new Response(200, {}, { foundUser, encodedToken });
     }
-    new Response(
-      401,
-      {},
-      {
-        errors: [
-          "The credentials you entered are invalid. Unauthorized access error.",
-        ],
-      }
+
+    return new Response(401, {},
+      { errors: ["The credentials you entered are invalid. Unauthorized access error."] }
     );
   } catch (error) {
-    return new Response(
-      500,
-      {},
-      {
-        error,
-      }
-    );
+    return new Response(500, {}, { error });
   }
 };
